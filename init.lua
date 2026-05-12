331acf90f6a0a0f932d76446f82ce034ae2129c2
Abdurrahman Shaikh <abdurrahmanmeshaikh@gmail.com>
2026-05-12 14:54:18 +0530

    attempting to fix tree sitter problem... hence backup

--- init.lua
+++ init.lua
@@ -25,19 +25,19 @@ require('lazy').setup({
 --    "MunifTanjim/nui.nvim",
 --    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode`
 --  },
-require 'plugins.neotree',
-require 'plugins.bufferline',
-require 'plugins.lualine',
+--require 'plugins.neotree',
+--require 'plugins.bufferline',
+--require 'plugins.lualine',
 require 'plugins.treesitter',
-require 'plugins.telescope',
-require 'plugins.lsp',
-require 'plugins.autocompletion',
-require 'plugins.gitsigns',
-require 'plugins.alpha',
-require 'plugins.indent-blankline',
-require 'plugins.misc',
+--require 'plugins.telescope',
+--require 'plugins.lsp',
+--require 'plugins.autocompletion',
+--require 'plugins.gitsigns',
+--require 'plugins.alpha',
+--require 'plugins.indent-blankline',
+--require 'plugins.misc',
 --require 'plugins.autoformatting'
-require 'plugins.colortheme(onedark)'
+--require 'plugins.moonfly'
 
 
 
